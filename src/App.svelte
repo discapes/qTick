4212661776319a42b45382b63ee70aa72a789999
Miika Tuominen <miika.km.tuominen@gmail.com>
2022-04-20 00:45:55 +0300

    time adjuster works but is buggy

--- src/App.svelte
+++ src/App.svelte
@@ -2,92 +2,86 @@
 	import Slider from "@bulatdashiev/svelte-slider";
 	import { spring, tweened } from "svelte/motion";
 	import Pie from "./Pie.svelte";
+	const size = 400;
+	const inset = 29;
+	const thickness = 50;
+	const root = document.documentElement;
 
 	const twoDigits = (n) => (n > 9 ? Math.floor(n) : "0" + Math.floor(n));
-	const hoursToHHMM = (h) => {
+	const HHMM = (h) => {
 		let H = Math.floor(h);
 		let M = (h * 60) % 60;
 		return `${twoDigits(H)}:${twoDigits(M)}`;
 	};
-	const HMToHours = (h, m) => h + m * 60;
 
-	const TimeObj = (name, color, start, end) => ({
+	const TimeObject = (name, color, range) => ({
 		name,
 		color,
-		start: start / 24,
-		end: end / 24,
+		range,
+		offset: 0,
+		get startTime() {
+			return this.range[0] % 24;
+		},
+		get endTime() {
+			return this.range[1] % 24;
+		},
+		get start() {
+			return ((this.range[0] + this.offset) % 24) / 24;
+		},
+		get end() {
+			return ((this.range[1] + this.offset) % 24) / 24;
+		},
 	});
-	const size = 400;
-	const inset = 29;
-	const thickness = 50;
 
-	const stuff = [
-		// tuesday
-		TimeObj("sleep", "blue", 		0+0		/60, 7+0	/60),
-		TimeObj("morning", "firebrick",	7+0		/60, 7+45	/60),
-		TimeObj("car", "darkorange",	7+45	/60, 8+25 	/60),
-		TimeObj("school", "red", 		8+30	/60, 14+45	/60),
-		TimeObj("eating", "hotpink", 	15+0	/60, 15+30	/60),
-
-		TimeObj("free", "lime", 		15+30	/60, 16+45	/60),
-
-		TimeObj("car", "darkorange", 	16+50	/60, 17+20	/60),
-		TimeObj("swimming", "green", 	17+20	/60, 19+15	/60),
-		TimeObj("car", "darkorange", 	19+15	/60, 19+45	/60),
-
-		TimeObj("free", "lime", 		20+0	/60, 22+0	/60),
-		TimeObj("eating", "hotpink", 	22+0	/60, 22+15	/60),
-		TimeObj("free", "lime", 		22+15	/60, 23+40	/60),
-		// monday
-		// TimeObj("sleep", "blue", 		0+0		/60, 7+0	/60),
-		// TimeObj("morning", "firebrick",	7+0		/60, 7+45	/60),
-		// TimeObj("car", "darkorange",	7+45	/60, 8+25 	/60),
-		// TimeObj("walking", "cyan",		8+25	/60, 8+30 	/60),
-		// TimeObj("school", "red", 		8+30	/60, 16+5	/60),
-		// TimeObj("car", "darkorange",	16+5	/60, 16+45 	/60),
-		// TimeObj("walking", "cyan",		16+45	/60, 16+55 	/60),
-		// TimeObj("eating", "hotpink", 	17+0	/60, 17+30	/60),
-
-		// TimeObj("free", "lime", 		17+30	/60, 18+15	/60),
-
-		// TimeObj("car", "darkorange", 	18+15	/60, 18+45	/60),
-		// TimeObj("swimming", "green", 	18+45	/60, 20+45	/60),
-		// TimeObj("car", "darkorange", 	20+45	/60, 21+15	/60),
-
-		// TimeObj("free", "lime", 		21+15	/60, 22+0	/60),
-		// TimeObj("eating", "hotpink", 	22+0	/60, 22+15	/60),
-		// TimeObj("free", "lime", 		22+15	/60, 24+0	/60),
-	];
-
-	let offset = 0;
-	for (let thing of stuff) {
-		if (thing.start > thing.end) {
-			offset = 24 - thing.start * 24;
+	function rotateTo0(tobjs) {
+		let offset = 0;
+		for (let tobj of tobjs) {
+			if (tobj.startTime > tobj.endTime) {
+				offset = 24 - tobj.startTime;
+			}
 		}
+		for (let tobj of tobjs) tobj.offset = offset;
+		return -offset;
 	}
-	for (let thing of stuff) {
-		thing.start += offset / 24;
-		thing.end += offset / 24;
-		if (thing.start == 24 / 24) thing.start = 0;
-	}
-	let slider = [offset * 4, 24 * 4];
-	$: offset = slider[0] / 4;
+
+	let tobjs = [TimeObject("test", "blue", [0, 16])];
+	let offset = 0;
+	$: offset = rotateTo0(tobjs);
 </script>
 
 <div
 	class="
-	 bg-gradient-to-t from-primary-500 to-secondary-500
+	 bg-gradient-to-t from-primary-900 to-secondary-900
 	 center
 	 h-screen
 	 overflow-auto"
 >
 	<div class="w-full max-w-xl">
-		<h1>Time visualizer</h1>
+		<h1 class="text-center">Time visualizer</h1>
 
-		offset: -{hoursToHHMM(offset)}
-		<div class="text-left w-full">
-			<Slider max={24 * 4} bind:value={slider} />
-		</div>
+		{#each tobjs as tobj}
+			{tobj.name}:
+			{HHMM(tobj.startTime)} -
+			{HHMM(tobj.endTime)}
+			<div
+				class="w-full"
+				style={`--progress-bg: ${
+					tobj.range[0] <= tobj.range[1] ? "#ff9355" : "white"
+				};
+			--track-bg: ${tobj.range[0] <= tobj.range[1] ? "white" : "#ff9355"};
+			`}
+			>
+				<Slider
+					step="0.25"
+					bind:value={tobj.range}
+					max="24"
+					range={tobj.range}
+				>
+					<span slot="left" style="font-size: 20px;">&#9193;</span>
+					<span slot="right" style="font-size: 20px;">&#9194;</span>
+				</Slider>
+			</div>
+		{/each}
 
 		<div class="flex">
 			<div
@@ -97,21 +91,21 @@
 				<span style="opacity:0.5">
 					<Pie
 						size={size - inset}
-						{stuff}
-						rotate={270 - (offset / 24) * 360}
+						{tobjs}
+						rotate={270 + (offset / 24) * 360}
 						{thickness}
 					/>
 				</span>
 			</div>
 			<div>
-				{#each stuff as thing, i}
-					{#if stuff.findIndex((e) => e.name == thing.name) == i}
+				{#each tobjs as tobj, i}
+					{#if tobjs.findIndex((e) => e.name == tobj.name) == i}
 						<div class="flex items-center text-xl">
 							<span
-								style={`background: ${thing.color};`}
+								style={`background: ${tobj.color};`}
 								class="inline-block w-8 h-8 m-1 border-2 rounded"
 							/>
-							{thing.name}
+							{tobj.name}
 						</div>
 					{/if}
 				{/each}
@@ -130,11 +124,11 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
-	html {
+	:root {
 		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
 			Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-		text-align: center;
 		color: white;
+		--thumb-bg: transparent;
 	}
 	* {
 		border-color: white;
--- src/Pie.svelte
+++ src/Pie.svelte
@@ -1,32 +1,34 @@
 <script>
-    export let size = 200;
-      export let thickness = 30;
-      export let stuff;
-      export let rotate = 270;
-      export let bg = "white";
-    
-    let viewBox = `0 0 ${size} ${size}`;
-    let radius = size / 2;
-    let halfCircumference = Math.PI * radius;
-      
-      for (let thing of stuff) {
-        let offset = halfCircumference * (thing.start);
-        thing.pieSize = halfCircumference * (thing.end - thing.start);
-        thing.dashArray = `0 ${offset} ${thing.pieSize} ${halfCircumference - thing.pieSize}`;
-      }
-  </script>
-  
-  <svg width={size} height={size} {viewBox}>
-  
-      {#each stuff as thing}
-            <circle
-      r={radius /2}
-      stroke={thing.color}
+  export let size = 200;
+  export let thickness = 30;
+  export let tobjs;
+  export let rotate = 270;
+  export let bg = "white";
+
+  let viewBox = `0 0 ${size} ${size}`;
+  let radius = size / 2;
+  let halfCircumference = Math.PI * radius;
+
+  $: {
+    for (let tobj of tobjs) {
+      let offset = halfCircumference * tobj.start;
+      tobj.pieSize = halfCircumference * (tobj.end - tobj.start);
+      tobj.dashArray = `0 ${offset} ${tobj.pieSize} ${
+        halfCircumference - tobj.pieSize
+      }`;
+    }
+  }
+</script>
+
+<svg width={size} height={size} {viewBox}>
+  {#each tobjs as tobj}
+    <circle
+      r={radius / 2}
+      stroke={tobj.color}
       stroke-width={radius}
-      stroke-dasharray={thing.dashArray}
-          transform={`translate(${radius} ${radius}) rotate(${rotate})`}
+      stroke-dasharray={tobj.dashArray}
+      transform={`translate(${radius} ${radius}) rotate(${rotate})`}
     />
-      
-      {/each}
-        <circle r={radius-thickness} cx={radius} cy={radius} fill={bg}/>
-  </svg>
+  {/each}
+  <circle r={radius - thickness} cx={radius} cy={radius} fill={bg} />
+</svg>
